e3828dedfa6b9026509164ce3f2ea01de1252663
flyer <flyer@flyerq.com>
2021-11-21 19:14:34 +0800

    feat: complete the popup page

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Alert, Button } from 'antd';
+import { SettingOutlined } from '@ant-design/icons';
 import './Popup.less';
 
 export default function Popup() {
   return (
     <div className="popup-page">
-      Popup Page
+      <Alert
+        type="info"
+        showIcon
+        message="Tips"
+        description={
+          <div>
+            <p>Options page can custom Shopify API params.</p>
+            <div>
+              <Button
+                icon={<SettingOutlined />}
+                type="primary"
+                onClick={() => chrome.runtime.openOptionsPage()}
+              >
+                Goto Options Page
+              </Button>
+            </div>
+          </div>
+        }
+      />
     </div>
   );
 };
