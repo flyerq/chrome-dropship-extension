381adba398ad3c2122e5b3c478c2abba4e3d0d06
flyer <flyer@flyerq.com>
2021-11-23 16:57:56 +0800

    chore: remove build directory git track

--- build/proxy-script.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("getProductData",(()=>{const t=(window.goodsDetailv2SsrData||{}).productIntroData||null;document.dispatchEvent(new CustomEvent("productData",{detail:t}))}));
